2f8a614e9c399f9f981af35fc587f759c5577139
bluewbear <bluew@naver.com>
2019-08-27 22:28:52 +0900

    Create ex41.scala
    ex41 연습 문제풀이

---
+++ ex41.scala
@@ -0,0 +1,49 @@
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = {
+    this match {
+      case None    => None
+      case Some(a) => Some(f(a))
+    }
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = {
+    this match {
+      case None    => None
+      case Some(a) => if (f(a) == None) None else f(a)
+    }
+  }
+
+  def getOrElse[B >: A](default: => B): B = {
+    this match {
+      case None    => default
+      case Some(a) => a
+    }
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = {
+    this match {
+      case None    => ob
+      case Some(a) => Some(a)
+    }
+  }
+
+  def filter(f: A => Boolean): Option[A] = {
+    this match {
+      case None              => None
+      case Some(a) if (f(a)) => Some(a)
+    }
+  }
+}
+
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+case class Employee(name: String, department: String)
+def lookupByName(name: String): Option[Employee] = {
+  if ("Joe" == name) Some(new Employee("Joe", "IT"))
+  else None
+}
+
+val joeDepartment: Option[String] = lookupByName("Joe").map(_.department)
+
+print(joeDepartment)
